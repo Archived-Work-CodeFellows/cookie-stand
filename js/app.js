b1b0561067348c891e0189db23e16a764b1b50a7
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-01-23 22:18:29 -0800

    Added Capitol Hill data

--- js/app.js
+++ js/app.js
@@ -159,6 +159,59 @@ var seaCenter = {
   }
 };
 
+var capHill = {
+  custMin: 20,
+  custMax: 38,
+  cookieAvg: 2.3,
+  custPerHour: [],
+  cookiePerHour: [],
+  totalCookies: 0,
+  customerPH: function() {
+    this.custPerHour.length = 14;
+    for(var i = 0; i < this.custPerHour.length; i++) {
+      this.custPerHour[i] = Math.ceil(Math.random()*(this.custMax-this.custMin)+this.custMin);
+    }
+    return this.custPerHour;
+  },
+  cookiePH: function() {
+    this.cookiePerHour.length = 14;
+    for(var i = 0; i < this.cookiePerHour.length; i++){
+      this.cookiePerHour[i] = Math.ceil(this.custPerHour[i]*this.cookieAvg);
+    }
+    return this.cookiePerHour;
+  },
+  cookieSum: function() {
+    for(var i = 0; i < this.cookiePerHour.length; i++) {
+      this.totalCookies += this.cookiePerHour[i];
+    }
+    return this.totalCookies;
+  },
+  writeToDOM: function() {
+    this.customerPH();
+    this.cookiePH();
+    this.cookieSum();
+
+    var heading = document.createElement('p');
+    var headContent = document.createTextNode('Capitol Hill');
+    heading.appendChild(headContent);
+    container.appendChild(heading);
+
+    var uList = document.createElement('ul');
+    for(var i = 0; i < this.custPerHour.length; i++) {
+      var list = document.createElement('li');
+      var listArr = document.createTextNode(this.cookiePerHour[i]);
+      list.appendChild(listArr);
+      uList.appendChild(list)[i];
+    }
+    container.appendChild(uList);
+    var bottom = document.createElement('p');
+    container.appendChild(bottom);
+    var bottomContent = document.createTextNode('Total: ' + this.totalCookies);
+    bottom.appendChild(bottomContent);
+  }
+};
+
 pike.writeToDOM();
 seaTac.writeToDOM();
 seaCenter.writeToDOM();
+capHill.writeToDOM();
