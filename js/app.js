3e9305c4214263ee38f69b7a5158a8421d2201ef
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-01-27 11:04:49 -0800

    Built table header function and finished table body render function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
 //define div container
 var container = document.createElement('div');
 document.body.appendChild(container);
+var table = document.createElement('table');
+table.setAttribute('id','cookie-data');
+container.appendChild(table);
 
 var locations = [];
 var hours =['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
@@ -30,12 +33,49 @@ function MakeStore(name,custMinPerH,custMaxPerH,cookieAvg) {
   };
   this.customerAndCookiePH();
 }
+MakeStore.prototype.render = function() {
+
+  var tRow = document.createElement('tr');
+  table.appendChild(tRow);
+  var thEl = document.createElement('th');
+  thEl.setAttribute('scope', 'row');
+  thEl.textContent = this.name;
+  tRow.appendChild(thEl);
+  var tData = [];
+  for(var i = 0; i < hours.length; i++){
+    tData[i] = document.createElement('td');
+    tData[i].textContent = this.cookiePerHour[i];
+    tRow.appendChild(tData[i]);
+  }
+  var totalEl = document.createElement('td');
+  totalEl.textContent = this.totalCookies;
+  tRow.appendChild(totalEl);
+};
+function headerGenerator() {
+  var tRow = document.createElement('tr');
+  table.appendChild(tRow);
+  var thEmpty = document.createElement('th');
+  tRow.appendChild(thEmpty);
+  var thEl = [];
+  for (var i = 0; i < hours.length; i++){
+    thEl[i] = document.createElement('th');
+    thEl[i].textContent = hours[i];
+    tRow.appendChild(thEl[i]);
+  }
+  var thEnd = document.createElement('th');
+  thEnd.textContent = 'Total Cookies';
+  tRow.appendChild(thEnd);
+}
 function populateLocations() {
   new MakeStore('First and Pike',23,65,6.3);
   new MakeStore('SeaTac Airport',3,24,1.2);
   new MakeStore('Seattle Center',11,38,3.7);
   new MakeStore('Capitol Hill',20,38,2.3);
   new MakeStore('Alki',2,16,4.6);
+  headerGenerator();
+  for(var k = 0; k < locations.length; k++){
+    locations[k].render();
+  }
 }
 populateLocations();
 
@@ -102,7 +142,7 @@ function createTable(){
   tData[15].textContent = locations[4].totalCookies;
   tRows[5].appendChild(tData[15]);
 }
-createTable();
+//createTable();
 
 // var pike = {
 //   custMin: 23,
